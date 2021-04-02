d862e7940dd6fb9272c05fe8df7dd377262ce479
Johnny <caijishiwo@gmail.com>
2021-04-02 15:36:59 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{ useState } from "react";
 function App() {
+  const [inputList, setInputList] = useState([{
+    firstName: "",
+    lastName: ""
+  }])
+  const handleInputChange = (e, index) =>{
+    const {name, value} = e.target;
+    const list = [...inputList];
+    list[index][name] = value;
+    setInputList(list);
+  }
+  const handleRemoveClick = index => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list)
+  }
+  const handleAddClick = ()=>{
+    setInputList([...inputList, { firstName: "", lastName: ""}])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        inputList.map((x, i)=>{
+          return(
+            <div className="box">
+              <input
+               name = "firstName"
+               value = {x.firstName}
+               onChange={ e => handleInputChange(e, i)}
+              />
+              <input
+               className="ml10"
+               name = "lastName"
+               value = {x.lastName}
+               onChange={ e => handleInputChange(e, i)}
+              />
+              <div>
+                {inputList.length !== 1 && <button className="mr10"
+                onClick={ ()=> handleRemoveClick(i)}
+                >Remove</button>}
+                {inputList.length - 1 === i && <button onClick={handleAddClick}>Add</button>}
+              </div>
+            </div>
+          )
+        })
+      }
+      <div style={{ marginTop: 20 }}>{JSON.stringify(inputList)}</div>
     </div>
   );
 }
